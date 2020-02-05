f03a412853f4f4460d6f424744f383573ffa21f9
Bernd Eissing <Bernd.Eissing@gmail.com>
2020-02-05 17:41:29 +0100

    Added damaging and dieing

--- GothicOneRemake/Assets/Scripts/EnemyScripts/EnemyController.cs
+++ GothicOneRemake/Assets/Scripts/EnemyScripts/EnemyController.cs
@@ -29,6 +29,9 @@ public class EnemyController : MonoBehaviour {
     public float wait_Before_Attack = 2f;
     private float attack_Timer;
 
+    [SerializeField]
+    private GameObject attack_Point;
+
     private Transform target;
 
     private void Awake() {
@@ -130,10 +133,12 @@ public class EnemyController : MonoBehaviour {
             enemy_Anim.Walk(false);
             enemy_State = EnemyState.ATTACK;
 
+
             //reset the chase distance to previous
             if(chase_Distance != current_Chase_Distance) {
                 chase_Distance = current_Chase_Distance;
             }
+
         } else if(Vector3.Distance(transform.position, target.position) > chase_Distance) {
             // player run away from enemy
 
@@ -151,25 +156,55 @@ public class EnemyController : MonoBehaviour {
         }
     } // chase
 
-    void Attack() { }
+    void Attack() {
+
+        navMeshAgent.velocity = Vector3.zero;
+        navMeshAgent.isStopped = true;
+
+        attack_Timer += Time.deltaTime;
+
+        if(attack_Timer > wait_Before_Attack) {
+            enemy_Anim.Attack();
+
+            attack_Timer = 0f;
+
+            //play attack sound
+        }
+
+        if(Vector3.Distance(transform.position, target.position) > attack_Distance + chase_After_Attack_Distance) {
+            enemy_State = EnemyState.CHASE;
+        }
+
+    } // attack
 
     void SetNewRandomDestination() {
 
         float rand_Radius = Random.Range(patrol_Radius_Min, patrol_Radius_Max);
 
         Vector3 randomDirection = Random.insideUnitSphere * rand_Radius;
-        print("Random point: " + randomDirection);
+
         // move the random position inside the patrol range
         randomDirection += transform.position;
 
-        print("Random local point: " + randomDirection);
         NavMeshHit navMeshHit;
 
         NavMesh.SamplePosition(randomDirection, out navMeshHit, rand_Radius, -1);
 
-        print("navMeshHit: " + navMeshHit.position);
-
         navMeshAgent.SetDestination(navMeshHit.position);
 
+    }
+
+    void Turn_On_AttackPoint() {
+        attack_Point.SetActive(true);
+    }
+
+    void Turn_Off_AttackPoint() {
+        if(attack_Point.activeInHierarchy) {
+            attack_Point.SetActive(false);
+        }
+    }
+
+    public EnemyState Enemy_State {
+        get; set;
     }
 } // class
