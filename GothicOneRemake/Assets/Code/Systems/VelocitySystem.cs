76065e5752357ca5b8519eda270f4f70d27bd6f8
Bernd Eissing <Bernd.Eissing@gmail.com>
2020-03-18 23:47:14 +0100

    Almost functioning collision prediction, Just need to wrap my head around calculating the penetration vector and removing that part from the velocity

--- GothicOneRemake/Assets/Code/Systems/VelocitySystem.cs
+++ GothicOneRemake/Assets/Code/Systems/VelocitySystem.cs
@@ -10,6 +10,7 @@ This system calculates the velocity for every entity in the game. Included
 in the velocity is gravity and player input.
 */
 // TODO: Look into TranslationSystemGroup and if you need to update after it
+[UpdateBefore(typeof(PlayerMovementSystem))]
 public class VelocitySystem : SystemBase {
 
     protected override void OnUpdate() {
@@ -76,41 +77,45 @@ public class VelocitySystem : SystemBase {
             // Cast a Sphere and see if it collides with anything 
             //####################################################################
 
-            Debug.DrawRay(translation.Value, direction, Color.green, 0.1f);
-
             // TODO: Put the whole SphereCast into the movement System or create a new system 
             // to handle collisions and calculate the correct velocity
-            var colPos = SphereCast(translation.Value, math.normalizesafe(translation.Value + direction, 0) * Time.DeltaTime, 0.5f);
+            var normal = SphereCast(translation.Value, translation.Value + math.normalizesafe(direction,0), 0.5f, math.normalizesafe(direction, 0));
 
-            if (math.length(colPos) > 0) {
+
+            if (math.length(normal) > 0) {
                 // TODO: Only here for debug reasons
-                direction -= colPos;
+                var pushOut = normal * direction;
+                Debug.Log("The push out Vector: " + pushOut + " and Velocity: " + direction);
+                direction -= pushOut;
                 //Debug.Log("Rewrite direction: " + colPos);
                 //direction = colPos;
             }
 
-            velocity.Value = math.normalizesafe(direction, 0);
+            velocity.Value = direction;
 
         }).WithoutBurst().Run();
     }
 
 
-    public unsafe float3 SphereCast(float3 RayFrom, float3 RayTo, float radius) {
+    public unsafe float3 SphereCast(float3 RayFrom, float3 RayTo, float radius, float3 debugRayDirection) {
+
+
+        Debug.DrawRay(RayFrom, debugRayDirection, Color.green);
 
         var physicsWorldSystem = World.DefaultGameObjectInjectionWorld.GetExistingSystem<BuildPhysicsWorld>();
         var collisionWorld = physicsWorldSystem.PhysicsWorld.CollisionWorld;
 
         var filter = new CollisionFilter() {
-            BelongsTo = 1u << 1, // Belongs to Layer 1
-            CollidesWith = 1u << 2, // Collides with Layer 2
+            BelongsTo = 1u, // Belongs to Layer 0 (Player)
+            CollidesWith = 1u << 2, // Collides with Layer 2 (Static Objects)
             GroupIndex = 0
         };
 
-        CapsuleGeometry sphereGeometry = new CapsuleGeometry() {
+        CapsuleGeometry geometry = new CapsuleGeometry() {
             Radius = radius
         };
 
-        BlobAssetReference<Unity.Physics.Collider> sphereCollider = Unity.Physics.CapsuleCollider.Create(sphereGeometry, filter);
+        BlobAssetReference<Unity.Physics.Collider> sphereCollider = Unity.Physics.CapsuleCollider.Create(geometry, filter);
 
         ColliderCastInput input = new ColliderCastInput() {
             Collider = (Unity.Physics.Collider*) sphereCollider.GetUnsafePtr(),
@@ -123,12 +128,23 @@ public class VelocitySystem : SystemBase {
 
         bool haveHit = collisionWorld.CastCollider(input, out hit);
         if (haveHit) {
-            var penetrationPoint = RayTo - hit.SurfaceNormal * 0.5f;
-            var penetration = hit.Position - penetrationPoint;
 
-            var moveOut = hit.SurfaceNormal * math.dot(penetration, hit.SurfaceNormal);
+            Debug.DrawRay(hit.Position, hit.SurfaceNormal, Color.red);
+            Debug.DrawRay(hit.Position, math.dot(RayTo - hit.Position, hit.SurfaceNormal) * hit.SurfaceNormal);
 
-            return moveOut;
+            return -hit.SurfaceNormal;
+
+            //var penetrationPoint = RayTo - hit.SurfaceNormal * 0.5f;
+            //var penetration = hit.Position - penetrationPoint;
+
+            //var moveOut = hit.SurfaceNormal * math.dot(penetration, hit.SurfaceNormal);
+
+
+            //////TODO: Only here for debug reasons
+            ////Debug.Log("The normal: " + hit.SurfaceNormal);
+            ////Debug.DrawRay(hit.Position, hit.SurfaceNormal, Color.red, 1f);
+
+            //return moveOut;
         }
 
         // Return null if we dont hit anything
